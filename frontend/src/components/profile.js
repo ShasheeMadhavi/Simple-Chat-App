f7df8946e9428f4004f70d13992cdc17d67ceab5
ShasheeMadhavi <92915@fhss.sjp.ac.lk>
2021-07-18 15:11:24 +0530

    sidebar implementation

---
+++ frontend/src/components/profile.js
@@ -0,0 +1,17 @@
+import '../styles/profile.scss';
+
+const Profile = () => {
+    return (
+        <div className="profile">
+            <div className="img-container">
+                <img alt="profile" src="" />
+            </div>
+            <div className="action-items">
+                login
+            </div>
+        </div>
+    )
+
+}
+
+export default Profile;
