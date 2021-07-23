f50e65f6e975245295eaf4aa68dcfc62e767cf35
ShasheeMadhavi <92915@fhss.sjp.ac.lk>
2021-07-23 16:04:36 +0530

    Login Input Sucess

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,40 @@ import Search from './components/search';
 import ChatList from './components/chatlist';
 import ChatSection from './components/chatsection';
 import Login from './components/login';
+import { BASE_URL, LOGIN } from './utils/apiEndpoints';
+import { postRequest } from './utils/apiRequests';
+import { useState } from 'react';
+
 
 
 function App() {
+  const [error, setError] = useState(null);
+  const [userObj, setUserObj] = useState(null);
+
+    const handleLogin = async (userData) => {
+      const formData = new FormData();
+      if(userData.file){
+        formData.append("profileImg", userData.file, userData.file.name);
+      }
+      formData.append("payload", JSON.stringify({name: userData.name}));
+
+      const response = await postRequest(`${BASE_URL}${LOGIN}`, formData);
+
+      console.log(response);
+
+      if(response.error){
+        setError(response.error);
+        return false;
+      }
+
+      setUserObj(response);
+      
+    }
+
   return (
     <>
       {true ? (
-        <Login />
+        <Login handleLogin={handleLogin} />
       ) : (
     <div className="App">
       <div className="sidebar">
--- frontend/src/utils/apiEndpoints.js
+++ frontend/src/utils/apiEndpoints.js
@@ -4,5 +4,5 @@ export const USER_LIST = "/api/user-list";
 export const USER = "/api/user";
 export const CHATS = "/api/chats";
 export const CHECK_IS_OFFLINE = "/api/user-is-offline";
-export const UPLOAD_AUDIO = "/api/uupload-voice";
+export const UPLOAD_AUDIO = "/api/upload-voice";
 export const UPLOAD_IMAGE_FILE = "/api/upload-image-file";
